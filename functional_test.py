a6aae853d5602bcd5eea23d1fab4feb8506d06aa
김진삼 <ssamkj@martmoa.com>
2017-11-03 16:42:47 +0900

    refactoringπ

--- functional_test.py
+++ functional_test.py
@@ -32,18 +32,14 @@ class NewVisitorTest(unittest.TestCase):
         # "1: 공작깃털 사기" 아이템이 추가된다.
         self.sendInputText('공작깃털 사기')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        rows = self.getTableRows()
         self.assertIn('1: 공작깃털 사기', [row.text for row in rows], '신규 작업이 테이블에 표시되지 않는다.')
 
         # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
         # 다시 "공작깃털을 이용해서 그물 만들기"라고 입력한다.(에디스는 매우 체계적인 사람이다.)
         self.sendInputText('공작깃털을 이용해서 그물 만들기')
         # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인다.
-
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        rows = self.getTableRows()
         self.assertIn('2: 공작깃털을 이용해서 그물 만들기', [row.text for row in rows], '신규 작업이 테이블에 표시되지 않는다.')
 
 
@@ -54,6 +50,11 @@ class NewVisitorTest(unittest.TestCase):
         # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다.
 
         # 만족하고 잠자리에 든다.
+
+    def getTableRows(self):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        return rows
 
     def sendInputText(self, inputText):
         inputbox = self.find_input_box()
