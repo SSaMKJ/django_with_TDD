0717d11d8e724f403ed3627c9d0212f353e0e142
김진삼 <ssamkj@martmoa.com>
2017-11-03 16:33:57 +0900

    redirect 와 화면에서 아이템 리스트 나오도록 수정.

--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 # Create your views here.
 
 
 def home_page(request):
-    text = request.POST.get('item_text', '')
-    print(len(text), text)
-    if len(text) > 0 :
+
+    if request.method == 'POST':
+        text = request.POST.get('item_text', '')
         textItem = Item()
         textItem.text = text
         textItem.save()
+        return redirect('/')
 
-    return render(request, 'home.html',{
-        'new_item_text':text
-    })
+
+    return render(request, 'home.html', {'items' : Item.objects.all()})
