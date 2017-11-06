f1df88adf01bd12ab8abb2a76457a81dec18fecc
김진삼 <ssamkj@martmoa.com>
2017-11-06 16:59:03 +0900

    사용자별 리스트 목록 만들도록 모든 테스트 케이스 성공.π

--- lists/views.py
+++ lists/views.py
@@ -14,10 +14,16 @@ def view_list(request, list_id):
     # 정규식으로 넘어오는 path variable 이 자동으로 list_id에 매핑되는구나... 좋네.
     list_ = List.objects.get(id=list_id)
     items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'items': items, 'list':list_})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
     return redirect('/lists/%d/'%(list_.id))
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
+    return redirect('/lists/%d/' % (list_.id))
